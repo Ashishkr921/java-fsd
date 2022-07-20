ef3d94c77e6a8effc10d504cff193e5783ed97ca
Ashishkr921 <ashishkr921@gmail.com>
2022-07-20 14:33:52 +0530

    first commit

--- Phase-1 Practice Project/Assisted Practice/Array.java
+++ Phase-1 Practice Project/Assisted Practice/Array.java
@@ -8,9 +8,18 @@ public class Array {
 		System.out.println("Elements of array a: "+a[i]);
 		}
 
-
+		//multidimensional array
+		int b[][]={{2,2,3},{8,4,5},{9,4,5}};
+		//Print the array elements
+		for(int i=0;i<3;i++){
+			System.out.println("\nElements of array b: ");
+		for(int j=0;j<3;j++){
+		System.out.print(b[i][j]+" ");
 		
 
 	}
-
+		System.out.println();
+		
+}
+	}
 }
